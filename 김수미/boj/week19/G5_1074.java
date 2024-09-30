7447cf090c2018ba7d607230cf25b61edbc3e343
suri-pu-bi <ymg12347@gmail.com>
2024-09-30 19:00:55 +0900

    Feat: [BOJ 1074] Z 문제 풀이

---
+++ 김수미/boj/week19/G5_1074.java
@@ -0,0 +1,55 @@
+/**
+ * Author    : Kim Su Mi
+ * Date      : 2024.09.30(Mon)
+ * Runtime   : 104 ms
+ * Memory    : 14216 KB
+ * Algorithm : Divide-and-conquer
+ */
+
+package 김수미.boj.week19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class G5_1074 {
+	public static void main(String [] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		int res = 0;
+		while (N !=0) {
+			N -= 1;
+
+			// 제 1사분면
+			if (r < Math.pow(2, N) && c < Math.pow(2, N)) {
+				res += 0;
+			}
+
+			// 제 2사분면
+			else if (r < Math.pow(2, N) && c >= Math.pow(2, N)) {
+				res += (int)(Math.pow(2, N) * Math.pow(2, N));
+				c -= (int) Math.pow(2, N);
+			}
+
+			// 제 3사분면
+			else if (r >= Math.pow(2, N) && c < Math.pow(2, N)) {
+				res += (int)(Math.pow(2, N) * Math.pow(2, N) * 2);
+				r -= (int) Math.pow(2, N);
+			}
+
+			// 제 4사분면
+			else {
+				res += (int)(Math.pow(2, N) * Math.pow(2, N) * 3);
+				r -= (int) Math.pow(2, N);
+				c -= (int) Math.pow(2, N);
+			}
+		}
+		System.out.println(res);
+	}
+}
